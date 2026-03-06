5a00db6adb2ff448457623f7dd3efdb2a34efcc4
AniketDhangar <21aniketdhangar13@gmail.com>
2026-03-06 10:36:22 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -17,12 +18,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/crypto_wallet")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.error("MongoDB Error:", err));
 
 app.use("/wallet", walletRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log("Server running on port 5000");
 });

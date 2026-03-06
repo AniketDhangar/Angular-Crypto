3808d8f85a368dcf3559b7c06f7738f1c7f4fd27
AniketDhangar <21aniketdhangar13@gmail.com>
2026-03-06 10:45:59 +0530

    fix render port and root route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const ecc = require('@bitcoinerlab/secp256k1');
-const bitcoin = require('bitcoinjs-lib');
+const ecc = require("@bitcoinerlab/secp256k1");
+const bitcoin = require("bitcoinjs-lib");
 
 bitcoin.initEccLib(ecc);
 
@@ -8,9 +8,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
-
-
 const walletRoutes = require("./routes/wallet.routes");
 
 const app = express();
@@ -18,12 +15,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error("MongoDB Error:", err));
+// root route (important for Render check)
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
 
 app.use("/wallet", walletRoutes);
 
-app.listen(PORT,"0.0.0.0", () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
